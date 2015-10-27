cc2ad62ad9988db1908096e49b0920113f1a685b
xiaerfei <xiaerfei1989@163.com>
2015-10-27 13:29:06 +0800

    init

---
+++ NetworingCenterDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NetworingCenterDemo
+//
+//  Created by xiaerfei on 15/10/27.
+//  Copyright (c) 2015年 RongYu100. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NetworingCenterDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  NetworingCenterDemo
+//
+//  Created by xiaerfei on 15/10/27.
+//  Copyright (c) 2015年 RongYu100. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
